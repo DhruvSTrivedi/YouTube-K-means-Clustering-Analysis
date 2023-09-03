7a4a08caf88f26d8aa1b064b467cb07f4a263591
Dhruv Trivedi <143839140+DhruvSTrivedi@users.noreply.github.com>
2023-09-03 16:18:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,61 @@
+# Import necessary libraries
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
 
-# Load the data
-def load_data(filepath, encoding='ISO-8859-1'):
-    return pd.read_csv(filepath, encoding=encoding)
+# 1. Load the Data
+youtube_data = pd.read_csv('Global YouTube Statistics.csv', encoding='ISO-8859-1')
 
-# One-hot encode categorical columns
-def encode_data(data, columns):
-    encoder = OneHotEncoder(drop='first', sparse=False)
-    return encoder.fit_transform(data[columns])
+# 2. Data Preprocessing
+# Extract the relevant columns for clustering: "video views" and "uploads"
+clustering_data = youtube_data[["video views", "uploads"]]
 
-# Perform the elbow test for optimal k
-def elbow_test(encoded_data, max_k=15):
-    k_values = range(1, max_k)
-    inertia_values = []
+# Scale the data to ensure both features have equal importance in clustering
+scaler = StandardScaler()
+scaled_data = scaler.fit_transform(clustering_data)
 
-    for k in k_values:
-        kmeans = KMeans(n_clusters=k, random_state=42)
-        kmeans.fit(encoded_data)
-        inertia_values.append(kmeans.inertia_)
+# 3. Elbow Test to Determine Optimal Number of Clusters
+sum_of_squared_distances = []
+K = range(1, 15)
+for k in K:
+    km = KMeans(n_clusters=k)
+    km = km.fit(scaled_data)
+    sum_of_squared_distances.append(km.inertia_)
 
-    plt.figure(figsize=(10, 6))
-    plt.plot(k_values, inertia_values, marker='o')
-    plt.title('Elbow Curve')
-    plt.xlabel('Number of Clusters (k)')
-    plt.ylabel('Inertia')
-    plt.grid(True)
-    plt.show()
+# Plotting the elbow curve
+plt.figure(figsize=(10, 6))
+plt.plot(K, sum_of_squared_distances, 'bx-')
+plt.xlabel('Number of clusters (k)')
+plt.ylabel('Sum of Squared Distances')
+plt.title('Elbow Method For Optimal k')
+plt.show()
 
-# Main function
-if __name__ == '__main__':
-    data = load_data('Global_YouTube_Statistics.csv')
-    encoded_data = encode_data(data, ['Youtuber', 'Title', 'Country'])
-    elbow_test(encoded_data)
+# 4. Apply k-means clustering with Optimal Clusters (in this case, 4 was chosen based on the elbow method)
+kmeans = KMeans(n_clusters=4, random_state=0)
+clusters = kmeans.fit_predict(scaled_data)
+
+# 5. Visualize the Clusters
+clustering_data['Cluster'] = clusters
+
+plt.figure(figsize=(12, 8))
+colors = ['red', 'blue', 'green', 'yellow']
+labels = ['Cluster 1', 'Cluster 2', 'Cluster 3', 'Cluster 4']
+
+for i in range(4):
+    plt.scatter(clustering_data['video views'][clustering_data.Cluster == i],
+                clustering_data['uploads'][clustering_data.Cluster == i], 
+                color=colors[i], 
+                label=labels[i])
+
+# Plot the centroids
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], 
+            s=300, c='black', marker='X', label='Centroids')
+
+plt.title('Clusters of YouTube Channels based on Views and Uploads')
+plt.xlabel('Video Views')
+plt.ylabel('Uploads')
+plt.legend()
+plt.grid(True)
+plt.show()
